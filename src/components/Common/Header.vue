f3443854721eb4cdd3da964bc7b88e8523a31e2b
muyichenran <961774108@qq.com>
2017-11-29 22:24:15 +0800

    添加首页

---
+++ src/components/Common/Header.vue
@@ -0,0 +1,144 @@
+<template>
+		<div>
+			  <div class="usa-header">
+						<div class="usa-h-center center1200 clearfix">
+							<div class="web-name">公司名称</div>							
+							<div class="usa-h-right">
+								<div class="sign-login">
+									<a title="登陆">登陆</a>
+									<a title="注册">注册</a>
+								</div>
+								<div class="our-order">
+									<a title="我的订单">我的订单</a>
+								</div>
+							</div>
+						</div>			
+				</div>
+				<div class="usa-meau">
+						<div class="center1200">
+							<ul class="meau-list">
+								<li class="meau-list-item">
+									<a>首页</a>
+								</li>
+								<li class="meau-list-item">
+									<a>供应商</a>
+								</li>
+								<li class="meau-list-item">
+									<a>衬衣</a>
+								</li>
+							</ul>
+						</div>
+				</div>
+		</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+		.usa-header{
+    width: 100%;
+    min-width: 1200px;
+//  position: fixed;
+//  top: 0;
+//  left: 0;
+    height: 40px;
+    line-height: 40px;
+    background-color: #000;
+    
+    z-index: 10;
+    .usa-h-center{
+        font-size: 16px;
+        .web-name{
+            float: left;
+            position: relative;
+            color: #666;
+        }
+        
+        .usa-h-right{
+            float: right;         
+            .sign-login{
+                float: left;
+                position: relative;
+                color: #666;
+                cursor: pointer;
+                a{
+                    display: inline-block;
+                    color: #666;
+                    padding: 0 10px;
+                    &:hover{
+                        color: #000;
+                    }
+                }
+            }
+            .our-order{
+                float: left;
+                color: #666;
+                cursor: pointer;
+                a{
+                    display: inline-block;
+                    position: relative;
+                    color: #666;
+                    padding: 0 10px;
+                    &:before{
+                        content: '';
+                        display: inline-block;
+                        position: absolute;
+                        left: 0;
+                        top: 13px;
+                        height: 16px;
+                        border-left: 1px solid #5c5c5c;
+                        vertical-align: middle;
+                    }
+                    &:hover{
+                        color: #000;
+                    }
+                }
+            }
+        }
+    }
+}
+
+.usa-meau{
+    width: 100%;
+    min-width: 1200px;
+    height: 60px;
+    background:#f4f0ea ;
+    box-shadow: 0 0 3px 0 rgba(0,0,0,.2);
+    .meau-list{
+        padding: 0 20px;
+        .meau-list-item{
+            float: left;
+            box-sizing: border-box;
+            display: inline-block;
+            padding: 0 20px 0;
+            &>a{
+                line-height: 60px;
+                color: #000000;
+                font-size: 20px;
+                font-weight: 700;
+                position:relative; 
+                display: block;
+                &:before{
+                    position: absolute;
+                    display: block;
+                    top: 0;
+                    width: 100%;
+                    height: 2px;
+                }
+                &:hover::before{                       
+                    background:#b4a078;                       
+                }
+            }
+        }
+    }
+}
+
+</style>
---
+++ src/components/Index/Index.vue
@@ -0,0 +1,376 @@
+<template>
+	<div class="main-content">
+		<div class="home-banner center1200 clearfix">
+			<div class="item">
+				<img src="img/1.jpg" />
+			</div>
+			<div class="item">
+				<img src="img/1.jpg" />
+			</div>
+			<div class="item">
+				<img src="img/1.jpg" />
+			</div>
+			<div class="item">
+				<img src="img/1.jpg" />
+			</div>
+		</div>	
+		<div class="new-product">
+				<div class="center1200 clearfix">
+					<div class="p-header">
+						<h3 class="name">新品首发</h3>
+					</div>
+					<div class="product-list clearfix">
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<span>爆品</span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<span>爆品</span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<span>爆品</span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<span>爆品</span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<span>爆品</span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<Span>爆品</Span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		<div class="new-product">
+				<div class="center1200 clearfix">
+					<div class="p-header">
+						<h3 class="name">新品首发</h3>
+					</div>
+					<div class="product-list clearfix">
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<Span>爆品</Span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<Span>爆品</Span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<Span>爆品</Span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<Span>爆品</Span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<Span>爆品</Span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+						<a class="list-item" title="男式咖啡碳+5℃升级保暖内衣套装">
+							<img src="img/1.jpg" />
+							<div class="list-text">
+								<div class="tags">
+									<Span>爆品</Span>
+								</div>
+								<div class="name">
+									<p>男式咖啡碳+5℃升级保暖内衣套装</p>
+								</div>
+								<p class="price">
+									<span>￥120</span>
+									<span class="tagName" >Adidas制造商</span>
+								</p>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>		
+	</div>		
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.main-content{
+    padding-top: 60px;
+    width: 100%;
+    min-width: 1200px;
+    margin: 0 auto;
+    .home-banner{
+        padding: 20px 0;
+        &>.item{
+            width: 285px;
+            height:285px;
+            float: left;
+            border: 1px solid #e5e5e5;
+            overflow: hidden;
+            margin-right: 20px;
+            img{
+                width: 100%;
+                -webkit-transition: -webkit-transform 1s;
+                -moz-transition: -moz-transform 1s;
+                -o-transition: -o-transform 1s;
+                transition: transform 1s
+            }
+            &:hover img{
+                -webkit-transform: scale(1.05);
+                -moz-transform: scale(1.05);
+                -ms-transform: scale(1.05);
+                -o-transform: scale(1.05);
+                transform: scale(1.05);
+                
+            }
+        }
+        &>.item:last-child{
+            margin-right: 0;
+        }
+    }
+    .new-product{
+        width: 100%;
+        min-width: 1200px;
+        padding: 30px 0;
+        background: #f4f0ea;
+        &:nth-child(odd)
+        {
+            background:#ffffff;
+            .list-item{
+                background: #f4f0ea;
+            }
+        }
+        &:nth-child(even)
+        {
+            background:#f4f0ea;
+            .list-item{
+                background: #fff;
+            }
+        }
+        .p-header{
+            padding-bottom: 20px;
+            .name{
+                font-size: 24px;
+                font-weight: 700;
+                line-height: 1;
+            }
+        }
+        .product-list{
+            .list-item{
+                float: left;
+                margin-bottom: 20px;
+                width: 224px;
+                margin-right: 20px;
+                border-radius: 3px;
+                display: block;
+                cursor: pointer;
+                
+                &:nth-child(5n+0){
+                    margin-right: 0;
+                }
+                &:hover img{
+                    -webkit-transform: scale(1.05);
+                   -moz-transform: scale(1.05);
+                    -ms-transform: scale(1.05);
+                    -o-transform: scale(1.05);
+                    transform: scale(1.05);
+                }
+                img{
+                    width: 100%;
+                    -webkit-transition: -webkit-transform 1s;
+                    -moz-transition: -moz-transform 1s;
+                    -o-transition: -o-transform 1s;
+                    transition: transform 1s
+                }
+                .list-text{
+                    padding: 10px 0;
+                    border-top: 1px solid #b3a07d;
+                }
+                .tags{
+                    height: 20px;
+                    font-size: 0;
+                    color: #fff;
+                    width: 200px;
+                    margin: 0 auto 3px;
+                    text-align: center;
+                    overflow: hidden;
+                    span{
+                        display: inline-block;
+                        height: 20px;
+                        line-height: 20px;
+                        color: #fff;
+                        padding: 0 5px;
+                        font-size: 12px;
+                        background-color: #F28F2D;
+                    }
+                }
+                .name{
+                    display: block;
+                    margin-bottom: 4px;
+                    font-weight: bold;
+                    height: 28px;
+                    font-size: 16px;
+                    line-height: 28px;
+                    padding: 0 10px;
+                    color: #333;
+                    p{
+                        overflow: hidden;
+                        white-space: nowrap;
+                        -ms-text-overflow: ellipsis;
+                        -o-text-overflow: ellipsis;
+                        text-overflow: ellipsis;
+                    }
+                }
+                .price{
+                    font-size: 14px;
+                    line-height: 18px;
+                    color: #d4282d;
+                    padding: 1px 0 9px;
+                    margin: 0;
+                    text-align: center;
+                    padding: 1px 0 9px;
+                    .tagName {
+                        background: 0 0;
+                        border-left: 1px solid #999;
+                        padding-left: 10px;
+                        margin-left: 7px;
+                        color: #999;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
